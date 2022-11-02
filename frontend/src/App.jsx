042c53bda30809b0cc39899d1fc47d5b6b81490e
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-11-02 18:03:58 +1100

    import bootstrap working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
 
+// Bootstrap Import
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App () {
-  return <></>;
+  return (
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+    </>
+  );
 }
 
 export default App;
