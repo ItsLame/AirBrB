fde3a5e7aa0dddae331c1a6f8c7f0b2d1f1f80f0
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-11-04 21:42:03 +1100

    Initial register screen using form component

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { CustomForm } from '../components/Form';
 
 export const Register = () => {
-  return <></>;
+  return (
+    <CustomForm
+      title="Register"
+      fields={[
+        // input: key, label, type, placeholder
+        ['register-email', 'Email', 'email', 'name@example.com'],
+        ['register-name', 'Name', 'text', 'exampleName'],
+        ['register-password', 'Password', 'password', 'examplePassword'],
+        [
+          'register-confirm-password',
+          'Confirm Password',
+          'password',
+          'examplePassword',
+        ],
+      ]}
+      buttons={[
+        // input: key, label
+        ['register-button', 'Register'],
+      ]}
+    />
+  );
 };
