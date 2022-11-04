171673ce923aa64078911765f96b605d597601e3
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-11-04 21:41:34 +1100

    Initial login screen using form component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { CustomForm } from '../components/Form';
 
 export const Login = () => {
-  return <></>;
+  return (
+    <CustomForm
+      title="Login"
+      fields={[
+        // input: key, label, type, placeholder
+        ['login-email', 'Email', 'email', 'name@example.com'],
+        ['login-password', 'Password', 'password', 'examplePassword'],
+      ]}
+      buttons={[
+        // input: key, label
+        ['login-button', 'Login'],
+      ]}
+    />
+  );
 };
