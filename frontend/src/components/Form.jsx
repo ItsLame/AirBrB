7a85368cb6d42962a11a3440ff8e4e1e6ef345f5
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-11-04 21:40:51 +1100

    Dynamic form component

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,72 +1,92 @@
 import React from 'react';
 
 // Bootstrap Import
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
-// import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Card from 'react-bootstrap/Card';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
+// Props (as per eslint)
+import PropTypes from 'prop-types';
 
-// export const CustomForm = ([[key, label, type, placeholder]]) => {
-export default class CustomForm extends React.Component {
-  constructor(props) {
-    super(props);
+export const CustomForm = ({ title, fields, buttons }) => {
+  CustomForm.propTypes = {
+    title: PropTypes.string,
+    fields: PropTypes.array,
+    buttons: PropTypes.array,
+  };
 
-    this.state = {
-      // key: props.key,
-      // label: props.label,
-    };
+  const [fieldList, setFieldList] = React.useState([]);
+  const [buttonList, setButtonList] = React.useState([]);
+
+  // TODO: maybe simplify the whole thing
+  // check length (to check if there's anything added/removed)
+  if (fieldList.length < fields.length) {
+    // temporary list
+    const tempFieldList = fieldList;
+
+    // add fields
+    for (let i = fieldList.length; i < fields.length; i++) {
+      const newField = (
+        <FloatingLabel
+          className="mb-3"
+          key={fields[i][0]}
+          // controlId="floatingInput"
+          controlId={fields[i][0]}
+          label={fields[i][1]}
+        >
+          <Form.Control type={fields[i][2]} placeholder={fields[i][3]} />
+        </FloatingLabel>
+      );
+
+      // FIXME: not important but useState (not refreshing) when add new element
+      tempFieldList.push(newField);
+      setFieldList(tempFieldList);
+    }
+  } else if (fieldList.length > fields.length) {
+    // temporary list
+    let tempFieldList = fieldList;
+
+    // gather keys
+    const fieldsKeys = fields.map((x) => x[0]);
+
+    // filter
+    tempFieldList = tempFieldList.filter((x) => fieldsKeys.includes(x[0]));
+    setFieldList(tempFieldList);
   }
 
-  render() {
-    return (
-      <Card>
-        <Card.Header as="h5">Login</Card.Header>
-        <Card.Body>
-          {/* {addField()} */}
-          {/* {fieldList} */}
-          asdf
-        </Card.Body>
-      </Card>
-    );
+  // add buttons
+  if (buttonList.length < buttons.length) {
+    // temporary list
+    const tempButtonList = buttonList;
+
+    // add buttons
+    for (let i = buttonList.length; i < buttons.length; i++) {
+      const newButton = <Button key={buttons[i][0]}>{buttons[i][1]}</Button>;
+
+      // FIXME: not important but useState (not refreshing) when add new element
+      tempButtonList.push(newButton);
+      setButtonList(tempButtonList);
+    }
+  } else if (buttonList.length > buttons.length) {
+    // temporary list
+    let tempButtonList = buttonList;
+
+    // gather keys
+    const buttonsKeys = buttons.map((x) => x[0]);
+
+    // filter
+    tempButtonList = tempButtonList.filter((x) => buttonsKeys.includes(x[0]));
+    setButtonList(tempButtonList);
   }
-}
-// export const CustomForm = () => {
-//   // const [fieldList] = React.useState([
-//   //   <FloatingLabel
-//   //     key="login-emailz"
-//   //     controlId="floatingInput"
-//   //     label="Email Address"
-//   //   >
-//   //     <Form.Control type="email" placeholder="name@example.com" />
-//   //   </FloatingLabel>,
-//   // ]);
 
-//   // const addField = () => {
-//   //   const tempFieldList = fieldList;
-
-//   //   const newField = (
-//   //     <FloatingLabel key={key} controlId="floatingInput" label={label}>
-//   //       <Form.Control type={type} placeholder={placeholder} />
-//   //     </FloatingLabel>
-//   //   )
-
-//   //   tempFieldList.push(newField);
-//   //   setFieldList(tempFieldList);
-//   //   console.log(tempFieldList)
-//   // }
-
-//   return (
-//     <>
-//       <Card>
-//         <Card.Header as="h5">Login</Card.Header>
-//         <Card.Body>
-//           {/* {addField()} */}
-//           {/* {fieldList} */}
-//           asdf
-//         </Card.Body>
-//       </Card>
-//     </>
-//   );
-// };
+  return (
+    <Card>
+      <Card.Header as="h5">{title}</Card.Header>
+      <Card.Body>
+        {fieldList}
+        {buttonList}
+      </Card.Body>
+    </Card>
+  );
+};
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import CustomForm from '../components/Form';
 
 export const Login = () => {
-  return (
-    <div>
-      asdf
-      <CustomForm />
-    </div>
-  );
+  return <></>;
 };
