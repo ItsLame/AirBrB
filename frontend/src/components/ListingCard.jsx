173c23a9376c39d6f164d29c989d9bc27223f7d3
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-11-07 09:15:39 +1100

    Listing Card component update + Landing page update
    + get details from api

--- frontend/src/components/ListingCard.jsx
+++ frontend/src/components/ListingCard.jsx
@@ -2,37 +2,86 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import { BsFillStarFill } from 'react-icons/bs';
+// import { MdRateReview } from 'react-icons/md';
+// import { MdOutlineRateReview } from 'react-icons/md';
 import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
 
-const ListingCard = () => {
-  // ListingCard.propTypes = {
-  //   token: PropTypes.string,
-  //   setToken: PropTypes.func,
-  // };
+const ListingCard = ({
+  title,
+  street,
+  city,
+  state,
+  country,
+  price,
+  reviews,
+  thumbnail,
+}) => {
+  ListingCard.propTypes = {
+    title: PropTypes.string,
+    street: PropTypes.string,
+    city: PropTypes.string,
+    state: PropTypes.string,
+    country: PropTypes.string,
+    price: PropTypes.string,
+    reviews: PropTypes.number,
+    thumbnail: PropTypes.string,
+    // reviews: PropTypes.array,
+  };
 
-  // const navigate = useNavigate();
+  const handleClick = () => {
+    console.log(`Click ${title}`);
+  };
 
   return (
     <>
-      <Button variant="outline-dark" className="mb-4 p-0 border-0 text-start">
-        <Card style={{ width: '300px' }} className="bg-transparent">
+      <Button
+        variant="outline-dark"
+        className="p-0 text-start h-100 w-100 overflow-auto"
+        // className="p-0 text-start w-100 overflow-auto"
+        onClick={handleClick}
+      >
+        <Card className="bg-transparent border-0">
           <Card.Img
             variant="top"
-            src="https://a0.muscache.com/im/pictures/miso/Hosting-54380902/original/9dc52173-f50d-47e5-b5d9-3c072ad7d40c.jpeg?im_w=960"
-            alt="place"
+            // src="https://a0.muscache.com/im/pictures/miso/Hosting-54380902/original/9dc52173-f50d-47e5-b5d9-3c072ad7d40c.jpeg?im_w=960"
+            src={thumbnail}
+            style={{
+              // width: '100%',
+              // height: '13rem',
+              height: '200px',
+              objectFit: 'cover',
+            }}
+            alt={`${title} thumbnail`}
+            className="bg-info"
           />
           <Card.Body>
             <Container className="d-flex p-0">
-              <Card.Title className="mt-2 flex-grow-1">Title</Card.Title>
-              <Card.Text className="d-flex gap-1 align-items-center">
+              <Card.Title
+                className="mt-2 flex-grow-1"
+                style={{
+                  width: '0',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                }}
+              >
+                {title}
+              </Card.Title>
+              <Card.Text className="d-flex gap-1 align-items-center w22">
                 <BsFillStarFill />
-                <span>{'1.23 (45)'}</span>
+                <span>{`0.00 (${reviews})`}</span>
+                {/* <MdRateReview size={20} /> */}
+                {/* <MdOutlineRateReview size={20} /> */}
+                {/* <span>{`${reviews}`}</span> */}
               </Card.Text>
             </Container>
             <Card.Text className="d-flex flex-column">
-              <span>UNSW Sydney High St</span>
-              <span>Kensington, NSW, Australia</span>
-              <span>$20 per night</span>
+              <span>{street}</span>
+              <span>
+                {city}, {state}, {country}
+              </span>
+              <span>${price} per night</span>
             </Card.Text>
           </Card.Body>
         </Card>
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -3,7 +3,12 @@ import { Outlet } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ListingCard from '../components/ListingCard';
 import Navbar from '../components/Navbar';
-// import Container from 'react-bootstrap/Container';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import { getListings } from '../services/listings';
 
 const Landing = ({ token, setToken }) => {
   Landing.propTypes = {
@@ -11,19 +16,47 @@ const Landing = ({ token, setToken }) => {
     setToken: PropTypes.func,
   };
 
+  const [listings, setListings] = React.useState([]);
+
+  React.useEffect(() => {
+    getListings().then((response) => {
+      // temporary list
+      let tempMap = [];
+
+      // map to list
+      tempMap = response.data.listings.map((x) => (
+        <Col key={x.title}>
+          <ListingCard
+            title={x.title}
+            street={x.address.street}
+            city={x.address.city}
+            state={x.address.state}
+            country={x.address.country}
+            price={x.price}
+            reviews={x.reviews.length}
+            thumbnail={x.thumbnail}
+          />
+        </Col>
+      ));
+
+      // sort alphabetically
+      tempMap = tempMap.sort((a, b) =>
+        a.props.children.props.title > b.props.children.props.title ? 1 : -1
+      );
+
+      // set temporary list to listings
+      setListings(tempMap);
+    });
+  }, []);
+
   return (
     <>
       <Navbar token={token} setToken={setToken} />
-      <main className="container d-flex mt-5 flex-wrap justify-content-between">
-        <ListingCard />
-        <ListingCard />
-        <ListingCard />
-        <ListingCard />
-        {/* <Container className="d-flex flex-wrap gap-3">
-          <div>Landing</div>
-          <ListingCardLanding />
-        </Container> */}
-      </main>
+      <Container className="my-5">
+        <Row xs={1} md={2} lg={3} xxl={4} className="g-4 h-100">
+          {listings}
+        </Row>
+      </Container>
       <Outlet />
     </>
   );
