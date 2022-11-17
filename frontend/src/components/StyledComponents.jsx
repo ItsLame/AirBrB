4128fec82c36c000ce644ae5f42b28ba73579c18
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-11-17 21:57:05 +1100

    update search toggle to styled components

--- frontend/src/components/StyledComponents.jsx
+++ frontend/src/components/StyledComponents.jsx
@@ -1,4 +1,5 @@
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import styled from 'styled-components';
 import { BsFillStarFill } from 'react-icons/bs';
 
@@ -21,5 +22,26 @@ export const StyledStarRating = styled(Card.Text)`
   &:hover {
     color: #ffc107;
     cursor: pointer;
+  }
+`;
+
+export const StyledSearchToggle = styled(Button)`
+  & {
+    color: #6c757d;
+    background: #1c1c1c;
+    box-shadow: inset 0 0 8px black, 0 0 0px black;
+    transition: box-shadow 150ms ease-in-out;
+  }
+
+  &:hover {
+    color: #89959f;
+    background: #1c1c1c;
+    box-shadow: inset 0 0 0px black, 0 0 8px black;
+  }
+
+  &:active:focus {
+    color: #89959f;
+    background: #1c1c1c;
+    transform: scale(0.98);
   }
 `;
--- frontend/src/components/listings/SearchToggle.jsx
+++ frontend/src/components/listings/SearchToggle.jsx
@@ -1,28 +1,7 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const StyledSearchToggle = styled(Button)`
-  & {
-    color: #6c757d;
-    background: #1c1c1c;
-    box-shadow: inset 0 0 8px black, 0 0 0px black;
-    transition: box-shadow 150ms ease-in-out;
-  }
-
-  &:hover {
-    color: #89959f;
-    background: #1c1c1c;
-    box-shadow: inset 0 0 0px black, 0 0 8px black;
-  }
-
-  &:active:focus {
-    color: #89959f;
-    background: #1c1c1c;
-    transform: scale(0.98);
-  }
-`;
+import { StyledSearchToggle } from '../StyledComponents';
 
 const SearchToggle = ({ onClickHandler }) => {
   SearchToggle.propTypes = {
