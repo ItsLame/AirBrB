46b850de6210d28814da50d929092ec947e467ec
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-11-18 00:19:09 +1100

    component testing: search form pt.1

--- frontend/src/tests/components/SearchForm.test.jsx
+++ frontend/src/tests/components/SearchForm.test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // import { render, screen } from '@testing-library/react';
 import { render } from '@testing-library/react';
-import ListingCard from '../../components/listings/ListingCard';
+import SearchForm from '../../components/listings/SearchForm';
 
 test('renders listing card component', () => {
-  render(<ListingCard />);
+  render(<SearchForm />);
+  // expect(screen.getByRole('button', { name: /clear all/i }));
+  // expect(screen.getByText(/filters/i));
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
 });
